9777136532d7ce5be95956716187ff9ce526f5b9
a8909 <boluwatifeshobola@gmail.com>
2024-12-25 17:11:30 +0100

    update the subscription plan and ui of the FAQS

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const webContact = document.querySelector('.web-contact');
 const webAbout = [{id: 1, content : 'Get your ready made WEBSITE with a little cost.'}, {id: 2, content : 'Get us build a scalable, functional and efficient WEB APPLICATION for your brand at a go.'}, {id: 3, content : 'With MY WEBSITE you can get your MOBILE APPLICATION built and live on playstore for commercial use.'}];
 const subscription = document.querySelectorAll('.web-subscription');
 const toggle = document.querySelector('.web-toggle');
-const webFeatureParentdiv = document.querySelector('.web-features')
+const webFeatureParentdiv = document.querySelector('.web-features');
+const webFaq = document.querySelector('.web-faqs');
+
 let display = false;
+let div = document.createElement('div');
+let idIndex = 0;
 webAbout.forEach((w) => {
     const aboutItem = document.createElement('div');
     aboutItem.classList.add('web-aboutitem')
@@ -20,7 +24,7 @@ function onAnimate() {
     webContent.classList.add('slideIn');
 }
 webContact.addEventListener('click',function directToWhatsapp(){
-    const whatsappUrl = '';
+    const whatsappUrl = 'https://wa.me/qr/TZVWNZ7JYEP6M1';
     window.open(whatsappUrl);
 });
 window.addEventListener('scroll', function move(){
@@ -33,19 +37,53 @@ window.addEventListener('scroll', function move(){
 subscription.forEach((plan) => {
     plan.addEventListener('click', function checkSubscription(){
     if(plan.textContent === 'Premium'){
+        let premium = document.querySelector('.web-subscription');
+        let standard = document.querySelector('.web-subscription.standard');
+        if (standard){
+            standard.classList.remove('standard');
+            premium.classList.add('standard');
+            updateSubscription('premium');
+        }
     }else{
         if(plan.textContent === 'Standard'){
             plan.classList.add('standard');
-            
+            let premium = document.querySelector('.web-subscription.standard');
+            if (premium){
+                premium.classList.remove('standard');
+            }
+            updateSubscription('standard');
         }
     }   
 })  
 })
 toggle.addEventListener('click', function showHide(){
     display =! display;
-    let div = document.createElement('div');
     if(display){
         toggle.textContent = 'close';
+        div.style.visibility = 'visibe';
+        subscription.forEach((plan)=>{
+            if(plan.textContent == 'Standard'){
+                updateSubscription('standard');
+            }
+            updateSubscription('premium');
+        } )
+    }else{
+        toggle.textContent = 'open';
+        subscription.forEach(plan=>{
+            if(plan.textContent == 'Premium' || plan.textContent == 'Standard'){
+                div.textContent = null;
+            }
+        })
+        
+    }
+})
+
+
+function updateSubscription(subcriptionPacakge){
+    let price = document.querySelector('.web-amount');
+    if(subcriptionPacakge == 'premium'){
+        price.textContent = '#10,000';
+        div.classList.add('features');
         const webFeatureslist = 
         `<ol>
         <li>Ability to change bought website template for free first time</li>
@@ -54,12 +92,45 @@ toggle.addEventListener('click', function showHide(){
         <button>Buy now</button>
         `
         div.innerHTML = webFeatureslist;
-        webFeatureParentdiv.append(div)
-      
+        webFeatureParentdiv.append(div);
     }else{
-        toggle.textContent = 'open'
-        if(div){
-            div = null;
-        }
-    }
+        price.textContent = '#50,000';
+        div.classList.add('features');
+        const webFeatureslist = 
+        `<ol>
+        <li>Ability to make changes to website design and also make unlimited changes offers by our developer</li>
+        <li>No charges for future and advance changes on the website</li>
+        <li>Once bought you are the sole owner of the website.</li>
+        <li>Access to FAQS features on the wesite</li>
+        <li>Easy access to connect your communication line e.g (WhatsApp, Email and any other service platform)</li></ol>
+        <button>Buy now</button>
+        `
+        div.innerHTML = webFeatureslist;
+        webFeatureParentdiv.append(div)
+        }  
+}
+
+const faqs = [{id: 1, question: "What is my website", answer : ""},{id: 2, question: "What does my webite have to offer", answer: ""},{id: 1, question: "What is my website", answer : ""},{id: 1, question: "What is my website", answer: ""},{id: 1, question: "What is my website",  answer: ""},]
+faqs.forEach((faq)=>{
+    let question = document.createElement('div');
+    question.classList.add('questions');
+    const answer = 'answer';
+    const option = 'question';
+    const questions = 
+    `<h6 class=${option}>${faq.question}</h6>
+    <span class= ${answer}>${faq.answer}</span>`;
+    question.innerHTML = questions;
+    webFaq.append(question);
 })
+
+// h6.forEach(answer => {
+//     answer.addEventListener('click', function showAnswer(){
+//         console.log('me')
+//         faqs.forEach((f, index)=>{
+//             idIndex = f[index].id;
+//             if(f[index].id == idIndex){
+//                 console.log('correct');
+//             }
+//         })
+//     })
+// });
